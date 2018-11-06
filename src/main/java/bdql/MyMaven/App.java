793fcf64fdfe169a2edfe03d9ff89e2ac0221c87
luoxin10086 <15928438186@163.com>
2018-11-06 15:09:14 +0800

    maven

---
+++ src/main/java/bdql/MyMaven/App.java
@@ -0,0 +1,13 @@
+package bdql.MyMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
